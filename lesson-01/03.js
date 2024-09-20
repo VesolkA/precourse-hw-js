6f35e758d50771bb58842cbb75269140099dab62
VesolkA <art.vesolka@gmail.com>
2024-09-20 15:07:27 +0300

    update task 3

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -4,7 +4,7 @@
 // и используя лишь
 // --number   number--  ++number   number++
 // Получи в консоли 1,3,3,1 
-// ❗Нужно вызвать консоль ЧЕТЫРЕ раза
+// Нужно вызвать консоль ЧЕТЫРЕ раза
 // Пример:
 // console.log(number**); 
 // console.log(**number); 
@@ -13,7 +13,7 @@
 
 let number = 1;
 
-console.log(number++); // Выведет 1, затем number станет 2
-console.log(++number); // Сначала увеличит до 3, затем выведет 3
-console.log(number--); // Выведет 3, затем number станет 2
-console.log(--number); // Сначала уменьшит до 1, затем выведет 1
+console.log(number++); 
+console.log(++number); 
+console.log(number--); 
+console.log(--number); 
